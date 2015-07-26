e524665e5ed5788a7fde251edaa9e05724612edd
zou3519 <zou3519@gmail.com>
2015-07-25 23:24:48 -0400

    revising the build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp')  
 var ts = require('gulp-typescript')
+var concat = require('gulp-concat')
 
 gulp.task('default', ['ts', 'watch'])
 
+gulp.task('ts-ng', function() {
+  gulp.src(['src/ng/**/*.ts'], {base: 'src'})
+    .pipe(ts({module: 'commonjs'}))
+    .js
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./wwwroot/public/js'))
+})
+
 // Compile typescript sources
-gulp.task('ts', function() {  
-    gulp.src(['src/**/*.ts'])
-        .pipe(ts({module: 'commonjs'}))
-        .js
-        .pipe(gulp.dest('./wwwroot'))
-});
+gulp.task('ts-server', function() {  
+  gulp.src(['src/controllers/**/*.ts','src/models/**/*.ts', 'src/*.ts'],
+    {base: 'src'})
+    .pipe(ts({module: 'commonjs'}))
+    .js
+    .pipe(gulp.dest('./wwwroot'))
+})
 
 gulp.task('watch', function() {  
-    gulp.watch('./src/**/*.ts', ['ts'])
-});
+  gulp.watch('./src/**/*.ts', ['ts'])
+})
--- src/server.ts
+++ src/server.ts
@@ -1,62 +1,12 @@
 /// <reference path="../typings/tsd.d.ts" />
 import express = require('express')
 import bodyParser = require('body-parser')
-import Recipe = require('./models/recipe')
-import mongoose = require('mongoose')
-import ObjectId = mongoose.Types.ObjectId
 
 var app = express()
 app.use(bodyParser.json())
+app.use('/api/recipes', require('./controllers/api/recipes'))
+app.use('/', require('./controllers/static'))
 
-app.get('/', function(req, res) {
-  res.sendfile('wwwroot/layouts/recipe.html')
-})
-
-app.get('/api/recipes/:id', function(req, res, next) {
-  var id: string = req.params.id
-  Recipe.find({_id: new ObjectId(id)}, function(err,results) {
-    if (err) { return next(err) }
-    if (results.length == 0) { 
-      res.json(404) 
-    } else {
-      res.json(results[0])
-    }
-  })
-})
-
-app.put('/api/recipes/:id', function(req, res, next) {
-  var id: string = req.params.id
-  var recipe = new Recipe({
-    name: req.body.name,
-    description: req.body.description,
-    cuisine: req.body.cuisine,
-    meal: req.body.meal,
-    ingredients: req.body.ingredients,
-    instructions: req.body.instructions,
-    _id: req.body._id
-  })
-  Recipe.findOneAndUpdate({_id: new ObjectId(id)}, recipe, {upsert:true},
-    function(err, recipe) {
-      if (err) { next(err) }
-      return res.json(recipe)
-    })
-})
-
-app.post('/api/recipes/', function(req, res, next) {
-  var recipe = new Recipe({
-    name: req.body.name,
-    description: req.body.description,
-    cuisine: req.body.cuisine,
-    meal: req.body.meal,
-    ingredients: req.body.ingredients,
-    instructions: req.body.instructions
-  })
-  console.log(recipe)
-  recipe.save(function (err, recipe) {
-    if (err) { return next(err) }
-    res.json(201, recipe)
-  })
-})
 app.listen(3000, function() {
   console.log("Server listening on", 3000)
 })
