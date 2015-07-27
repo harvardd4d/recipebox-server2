64098e691f3d8f4e41dff868dfdf9ccbe3133253
zou3519 <zou3519@gmail.com>
2015-07-27 01:42:54 -0400

    error

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,11 @@
 /// <reference path="../typings/tsd.d.ts" />
 import mongoose = require('mongoose')
 var url = process.env.MONGOLAB_URI || 'mongodb://localhost/recipebox'
-mongoose.connect(url, function () {
-  console.log('mongodb connected')
+mongoose.connect(url, function (err) {
+  if (err) {
+    console.log('could not connect to mongodb', err)
+  } else {
+    console.log('mongodb connected')
+  }
 })
 export = mongoose
