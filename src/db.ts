e6712bad9704cf77bf0c59f1b5fa78073410ccfe
zou3519 <zou3519@gmail.com>
2015-07-27 23:51:10 -0400

    some cosmetic changes

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,11 @@
 /// <reference path="../typings/tsd.d.ts" />
 import mongoose = require('mongoose')
-console.log("attempting to load mongoose")
 var url = process.env.MONGOLAB_URI || 'mongodb://localhost/recipebox'
 mongoose.connect(url, function (err) {
   if (err) {
-    console.log('could not connect to mongodb', err)
+    console.log('Could not connect to mongodb:', err)
   } else {
-    console.log('mongodb connected')
+    console.log('Connected to mongodb')
   }
 })
 export = mongoose
